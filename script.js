2e034c92ad9b1c02bfd758dba28c1f4e3cf637d2
darenm9 <darenjmc@gmail.com>
2024-07-31 11:38:34 -0400

    not finished

---
+++ script.js
@@ -0,0 +1,21 @@
+let title = document.getElementById("title_input");
+let sentence = document.getElementById("story_result");
+let mainTitle = document.getElementById("main_title");
+let noun = document.getElementById("noun");
+let verb = document.getElementById("verb");
+let adjective = document.getElementById("adjective");
+let submitButton = document.getElementById("submit_button");
+
+
+function popUp(event){
+
+    event.preventDefault();
+
+    let nounInput = noun.value
+    let verbInput = verb.value
+    let adjectiveInput = adjective.value
+    let titleInput = title.value
+
+    
+}
+
